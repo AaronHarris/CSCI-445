74e1ce24330f422b5e1084a1cea66bf7f8ba91fa
freyconner24 <frey.conner24@gmail.com>
2015-04-06 19:55:31 -0700

    Fixed TestTurn with string stuff array stuff

--- TestTurning/testservo.cpp
+++ TestTurning/testservo.cpp
@@ -1,6 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "wiringPi.h"
+
+void turnLeft(FILE *fp, int time)
+{
+    //printf("turn left"); 
+    puts("Update simulated position to turn left");
+    fprintf(fp,"0=141\n");//turn right
+    fprintf(fp,"1=142\n");
+    fflush(fp);
+    delay(time); //600
+    fprintf(fp, "0=151\n");//stopped - reach middle
+    fprintf(fp, "1=152\n");
+    fflush(fp);
+
+}
+
+void turnRight(FILE *fp, int time)
+{
+    puts("Update simulated position to turn right");
+
+    fprintf(fp,"0=163\n");//turn right
+    fprintf(fp,"1=162\n");
+    fflush(fp);
+    delay(time); //600
+    fprintf(fp, "0=151\n");//stopped - reach middle
+    fprintf(fp, "1=152\n");
+    fflush(fp);
+
+}
+
+void moveForward(FILE *fp, int time)
+{
+    puts("Update simulated position to move forward");
+
+    printf("move forward"); 
+    fprintf(fp,"0=163\n");//moves forward
+    fprintf(fp,"1=142\n");
+    fflush(fp);
+    delay(time);
+    fprintf(fp, "0=151\n");//stopped - reach middle
+    fprintf(fp, "1=152\n");
+    fflush(fp);
+}
+
+void turnBackward(FILE *fp, int time)
+{
+
+    puts("Update simulated position to move backward");
+    fprintf(fp,"0=163\n");//turn right
+    fprintf(fp,"1=162\n");
+    fflush(fp);
+    delay(time); //1400
+    fprintf(fp, "0=151\n");//stopped
+    fprintf(fp, "1=152\n");
+    fflush(fp);
+}
+
+
+
 int main(void)
 {
     FILE *fp;
@@ -12,7 +70,7 @@ int main(void)
 
     int turn = -1;
     printf("choose turn to diagnose (1-4): Left, Right, Forward, Backward:");
-    string direction[4] = {"Left", "Right", "Forward","Backward"};
+    const char* direction[] = {"Left", "Right", "Forward","Backward"};
     scanf("%d", &turn);
 
     int val = -1;
